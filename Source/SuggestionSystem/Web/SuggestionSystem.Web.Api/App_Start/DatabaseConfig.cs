1bbba9e4c6df2c07d6d2822ca916bcd2a32ca025
Hristo Stoichev <hrist0.stoichev@mail.bg>
2016-10-07 00:08:07 +0300

    Migrations enabled

---
+++ Source/SuggestionSystem/Web/SuggestionSystem.Web.Api/App_Start/DatabaseConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuggestionSystem.Web.Api.App_Start
+{
+    public class DatabaseConfig
+    {
+    }
+}
