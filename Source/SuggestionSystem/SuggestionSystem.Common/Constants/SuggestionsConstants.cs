8379257b5f44d38cb9167b4b37643b97906c5eea
Hristo Stoichev <hrist0.stoichev@mail.bg>
2016-10-10 16:07:13 +0300

    Constants extracted

---
+++ Source/SuggestionSystem/SuggestionSystem.Common/Constants/SuggestionsConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace SuggestionSystem.Common.Constants
+{
+    public class SuggestionsConstants
+    {
+        public const int TitleMinLength = 20;
+        public const int TitleMaxLength = 50;
+
+        public const int ContentMinLength = 30;
+        public const int ContentMaxLength = 200;
+    }
+}
