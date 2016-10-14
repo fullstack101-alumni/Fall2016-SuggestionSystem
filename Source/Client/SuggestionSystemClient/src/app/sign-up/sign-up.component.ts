9d9945c865e0e5ff4271e088a0235396649ad589
Plamen Duchev <p2duchev@abv.bg>
2016-10-14 03:46:14 +0300

    Creating signing up form

--- Source/Client/SuggestionSystemClient/src/app/sign-up/sign-up.component.ts
+++ Source/Client/SuggestionSystemClient/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SuggestionboxaubgApiService } from '../suggestionboxaubg-api.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  email: string
+  password: string
+  confirmPassword: string
 
-  constructor() { }
+
+  constructor(private _suggestionBoxAubgApiService:SuggestionboxaubgApiService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this._suggestionBoxAubgApiService.registerUser(this.email, this.password, this.confirmPassword)
+      .subscribe(
+        data => {
+          this.route.navigate="/login";
+          error => console.log('Error fetching stories'))
+        }
+      )
   }
 
 }
