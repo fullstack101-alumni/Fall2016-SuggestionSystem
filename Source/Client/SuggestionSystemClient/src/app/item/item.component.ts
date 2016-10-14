1a5ea6c60a4d7868cbfc264c9b4d679c4e6ba707
hrist0stoichev <hrist0.stoichev@mail.bg>
2016-10-15 00:16:00 +0300

    comments visualization implemented

--- Source/Client/SuggestionSystemClient/src/app/item/item.component.ts
+++ Source/Client/SuggestionSystemClient/src/app/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { SuggestionboxaubgApiService } from '../suggestionboxaubg-api.service';
 
 @Component({
   selector: 'item',
@@ -8,12 +9,23 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ItemComponent implements OnInit {
   @Input() item;
   panelColor: string;
+  comments;
+  numberOfCommentsToGet: number;
 
-  constructor() {}
+  constructor(private _suggestionBoxAubgApiService:SuggestionboxaubgApiService) {}
 
   ngOnInit() {
     var colors: string[] = ["warning", "primary", "default", "success", "danger"];
     this.panelColor = colors[this.item.Status];
+    this.numberOfCommentsToGet = 1;
+    this.comments = [];
   }
 
+  getComments() {
+    this._suggestionBoxAubgApiService.fetchComments(this.item.Id, this.comments.length, this.numberOfCommentsToGet)
+    .subscribe(
+      items => {for(var i=0; i < items.length; i++) this.comments.push(items[i])},
+      error => console.log("Error fetching comments")
+    );
+  }
 }
