d3df96a4440da8eefb2d49daa355852c650f2dcf
hrist0stoichev <hrist0.stoichev@mail.bg>
2016-10-20 20:31:35 +0300

    Adding comment and suggestion models

---
+++ Source/Client/SuggestionSystemClient/src/app/models/suggestion.ts
@@ -0,0 +1,11 @@
+export class Suggestion {
+  Id: number;
+  Title: string;
+  Content: string;
+  UpVotesCount: number;
+  DownVotesCount: number;
+  CommentsCount: number;
+  DateCreated: any;
+  Author: string;
+  Status: number;
+}
